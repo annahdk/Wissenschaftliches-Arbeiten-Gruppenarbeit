6be431620eadaa514c04d09a03e0241c20414ef3
ceotimritter <78644108+ceotimritter@users.noreply.github.com>
2021-02-12 18:02:53 +0100

    Add files via upload
    Ich hab schon mal die Variable Alter definiert.
    Ich denke wir sollten die anderen Teilaufgaben erstmal separat machen und dann über Verbesserungen diskutieren.

---
+++ Aufgabe1-GitHub.R
@@ -0,0 +1,21 @@
+## Aufgabe 1
+
+# a)
+# Alter
+
+Alter <- rnorm(100, mean = 25, sd = 2)
+
+# b)
+# Studienfach
+
+
+# c)
+# Interesse an Mathematik
+
+
+# d)
+# Interesse an Programmieren
+
+
+# e)
+# Mathe - LK
