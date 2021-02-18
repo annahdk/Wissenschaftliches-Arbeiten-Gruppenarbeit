76c608a854c2c9443fefdf96e57afa4695e3aee3
leia-b <75613323+leia-b@users.noreply.github.com>
2021-02-18 11:30:36 +0100

    Update Hilfsfunktionen.R

--- Hilfsfunktionen.R
+++ Hilfsfunktionen.R
@@ -1,1 +1,22 @@
 ## Hilfsfunktionen
+
+## Phi-Streuungsmaß:
+
+phi_str <- function(x){
+  if(!is.factor(x)) stop("Funktion erwartet Faktor")
+  
+  ## Häufigkeitsverteilung der Levels:
+  K <- length(levels(x)) 
+  hk <- numeric(K)
+  l <- length(x)
+  for (i in 1:K) {
+    s <- sum(x == levels(x)[i])
+    hk[i] <- s/l
+  }
+  
+  ## Berechnung des Phi-Streuungsmaß
+  phi_min <- 2*(1-hk[which.max(table(x))])
+  phi_max <- sum(abs(hk-1/K))
+  
+  phi_min/(phi_min+phi_max)
+}
