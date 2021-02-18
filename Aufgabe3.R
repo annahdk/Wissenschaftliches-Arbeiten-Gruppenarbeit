bc4fa34b4f08e6dfd3e7377f003c84b33bf1d9a6
leia-b <75613323+leia-b@users.noreply.github.com>
2021-02-18 11:53:33 +0100

    Update Aufgabe3.R

--- Aufgabe3.R
+++ Aufgabe3.R
@@ -54,26 +54,6 @@ deskr_kat <- function(x, ...){
   else stop("Funktion erwartet Faktor")
 }
 
-## Hilfsfunktion Phi-Streuungsmaß
-
-phi_str <- function(x){
-  if(!is.factor(x)) stop("Funktion erwartet Faktor")
-  
-  ## Häufigkeitsverteilung der Levels:
-  K <- length(levels(x)) 
-  hk <- numeric(K)
-  l <- length(x)
-  for (i in 1:K) {
-    s <- sum(x == levels(x)[i])
-    hk[i] <- s/l
-  }
-  
-  ## Berechnung des Phi-Streuungsmaß
-  phi_min <- 2*(1-hk[which.max(table(x))])
-  phi_max <- sum(abs(hk-1/K))
-  
-  phi_min/(phi_min+phi_max)
-}
 
 ## Beispiele:
 
