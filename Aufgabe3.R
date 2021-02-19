7a64926913143a01470696b6abd74f358654461c
ceotimritter <78644108+ceotimritter@users.noreply.github.com>
2021-02-19 08:55:10 +0100

    Aufgabe3.R
    Ich hab jetzt auch die d) hinzugefügt.

--- Aufgabe3.R
+++ Aufgabe3.R
@@ -68,6 +68,45 @@ deskr_kat(housing$Type)
 #c)
 
 #d)
+## Eine Funktion, die geeignete deskriptive bivariate Statistiken fuer
+## den Zusammengang zwischen einer metrischen und einer
+## dichotomen Variablen berechnet und ausgibt
+
+a <- read.csv("Datensatz.csv")
+
+# x = dichotom
+# y = metrisch
+
+deskr_d <- function(x, y){
+  cat(" Deskriptive Statistiken: \n",
+      "------------------------\n",
+      "Arithm. Mittel (Ja):", mean(y[x == "Ja"]), "\n",
+      "Arithm. Mittel (Nein):", mean(y[x == "Nein"]), "\n",
+      "Median (Ja):", median(y[x == "Ja"]), "\n",
+      "Median (Nein):", median(y[x == "Nein"]), "\n",
+      "Varianz (Ja):", var(y[x == "Ja"]), "\n",
+      "Varianz (Nein):", var(y[x == "Nein"]), "\n",
+      "Std.abw.(Ja):", sd(y[x == "Ja"]), "\n",
+      "Std.abw.(Nein):", sd(y[x == "Nein"]), "\n",
+      "Range (Ja):", range(y[x == "Ja"]), "\n",
+      "Range (Nein):", range(y[x == "Nein"]), "\n",
+      "Quantile : \n")
+  for(i in 1:5){
+    p <- c("0% (für Ja):  ", "25%: ", "50%: ", "75%: ", "100%:")
+    cat("  ", p[i] , quantile(y[x == "Ja"])[i], "\n")
+  }
+  cat("\n",
+  for(i in 1:5){
+    p <- c("0% (für Nein):  ", "25%: ", "50%: ", "75%: ", "100%:")
+    cat("  ", p[i] , quantile(y[x == "Nein"])[i], "\n")
+  })
+  cat(" ------------------------\n")
+  plot(x,y)
+  addmargins(table(x,y))
+}
+deskr_d(a$Mathe.LK,a$Intersse.an.Mathematik)
+
+
 
 #e)
 
